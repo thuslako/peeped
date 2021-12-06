c5627297e93f346ea684f6437ef2626152ceb522
Luk <lukundu.lako@gmail.com>
2021-12-06 00:34:59 -0500

    update: enabled test fetch to serverless function

--- netlify/functions/twitter/twitter.ts
+++ netlify/functions/twitter/twitter.ts
@@ -1,27 +1,29 @@
-import {Handler} from '@netlify/functions'
 import fetch from 'node-fetch'
+import {Handler} from '@netlify/functions'
+
 
 const handler : Handler = async (event,context) =>{
   const id = event.queryStringParameters.id
-  const  result = await fetch(`https://api.twitter.com/2/tweets/${id}`,{
+
+  const result = await fetch(`https://api.twitter.com/2/tweets/${id}`,{
     method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${process.env.TWITTER_TOKEN}`,
-      }
-  })
-  const  {errors,data} = await result.json()
-  if(errors){
-    return {
-      statusCode: 400,
-      body: JSON.stringify(errors[0])
+    headers: {
+      'Authorization': `Bearer ${process.env.TWITTER_TOKEN}`,
     }
-  }
+  })
+  const  {data,errors} = await result.json()
   if(data){
     return {
       statusCode: 200,
       body: JSON.stringify({data})
     }
   }
+  else{
+    return {
+      statusCode: 400,
+      body: JSON.stringify(errors[0])
+    }
+  }
 }
 
-export {handler}
+export { handler }
--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,37 @@
 <script lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import {defineComponent} from 'vue'
 import Peeped from './components/Peeped.vue'
 
-export default {
-  components: {Peeped},
+export default defineComponent({
+  components:{
+    Peeped
+  },
   setup(){
     return Peeped
   },
   data(){
     return {
       query: "",
-      payload: {text:"",user:""}
+      payload: {text:"Paste Tweet URL,then generating image!",user:"@thuslako"}
     }
   },
   methods:{
-    async fetchTweet(){
-      if(this.query !== "" ){
-        const { data } = await fetch(`/.netlify/functions/twitter/?id=${this.getTweetId(this.query)}`)
-        if(data.text) this.payload.text = data.text
-        else {
-          this.payload = {
-            text:"Paste Tweet URL,then generating image",
-            user: "@thuslako"
-          } 
-        }
-      }
-    }
-  },
-  computed:{
-    getTweetId(){
-        if(this.query) this.query.match('/([^?s=\d]([^\/][\d]+))/g').split('/')[0]
-      
-    }
+     async fetchTweet(){
+      const result = await fetch(`/.netlify/functions/twitter/?id=1467199059614220290`)
+      const {data} = await result.json()
+      // const payload = await {text: data.text, user: '@unknown'}
+      this.payload = await {text: data.text, user: '@unknown'}
+    },
+    // getTweetId: ()=>{
+    //  "https://twitter.com/sheluvhec/status/1467199059614220290?s=20".match('/([^?s=\d]([^\/][\d]+))/g').split('/')[0]
+    // }
   },
   watch:{
     query: 'fetchTweet'
   }
-}
+})
 </script>
 
 <template>
@@ -47,11 +41,11 @@ export default {
   <div class="w-10/12 md:w-[45%] mt-10 mx-auto">
     <div class="w-full flex justify-between h-12">
       <input type="search" class="w-full rounded-l-xl bg-green-50 border-2 border-green-400  px-4 text-xl text-green-800 focus:ring-4 focus:ring-green-100 focus:ing-opacity-50" name="URI" id="">
-      <button class="rounded-r-xl border-2 bg-green-400 text-green-900" type="submit">Generate</button>
+      <button  class="rounded-r-xl border-2 bg-green-400 text-green-900" @click="fetchTweet" type="submit">Generate</button>
     </div>
   </div>
   <section class="py-4">
-    <Peeped :tweet="{text:'Paste Tweet URL,then generating image',user:'@thuslako'}"/>
+    <Peeped :tweet="payload"/>
   </section>
 </template>
 
