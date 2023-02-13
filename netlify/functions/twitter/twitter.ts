6b5abe74cacfc7f1691ce7a658796555528a8248
Luk <lukundu.lako@gmail.com>
2023-02-13 13:24:51 -0500

    update: fetch url

--- netlify/functions/twitter/twitter.ts
+++ netlify/functions/twitter/twitter.ts
@@ -5,7 +5,7 @@ import { Handler } from '@netlify/functions'
 const handler: Handler = async (event, context) => {
   const id = event.queryStringParameters.id
 
-  const result = await fetch(`https://api.twitter.com/2/tweets/${id}&expansions=attachments.media_keys0&expansions=attachments.media_keys&media.fields=duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width,alt_text`, {
+  const result = await fetch(`https://api.twitter.com/2/tweets?ids=${id}&expansions=attachments.media_keys0&expansions=attachments.media_keys&media.fields=duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width,alt_text`, {
     method: 'GET',
     headers: {
       'Authorization': `Bearer ${process.env.TWITTER_TOKEN}`,
@@ -19,11 +19,11 @@ const handler: Handler = async (event, context) => {
     }
   }
 
-  const { data, errors } = await result.json()
-  if (data) {
+  const { data, includes, errors } = await result.json()
+  if (data || includes) {
     return {
       statusCode: 200,
-      body: JSON.stringify({ data })
+      body: JSON.stringify({ data, includes })
     }
   }
   if (errors) {
