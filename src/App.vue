ab9ea7103b6fe9ff5a0aec6123753a52bc96ad77
Luk <lukundu.lako@gmail.com>
2023-02-13 12:54:37 -0500

    update: cleaned up filtering

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,39 @@
 <script lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import {defineComponent} from 'vue'
+import { defineComponent } from 'vue'
 import Peeped from './components/Peeped.vue'
 
 export default defineComponent({
-  components:{
+  components: {
     Peeped
   },
-  setup(){
+  setup() {
     return Peeped
   },
-  data(){
+  data() {
     return {
       query: "",
-      payload: {text:"Paste Tweet URL,then generating image!",user:"@thuslako"}
+      payload: { text: "Paste Tweet URL,then generating image!", user: "@thuslako" }
     }
   },
-  methods:{
-     async fetchTweet(){
+  methods: {
+    async fetchTweet() {
       const result = await fetch(`/.netlify/functions/twitter/?id=${this.getTweetID(this.query)}`)
-      const {data} = await result.json()
-      this.payload = await {text: data.text, user: `@${this.getTweetUser(this.query)}`}
-    }, 
-    getTweetUser(query:string){
+      const { data } = await result.json()
+      this.payload = await { text: data.text, user: `@${this.getTweetUser(this.query)}` }
+    },
+    getTweetUser(query: string) {
       const user = query.match(/([^/])\w+/g)
-      if(user[3]) return user[3]
+      if (user?.length) return user[3]
       else return 'unknown'
-    },   
-    getTweetID(query:string){
-     const id = query.match(/([^?s=\d]([^\/][\d]+))/g)
-     if(id)return id?.toString()?.split('/')[1]
+    },
+    getTweetID(query: string) {
+      const id = query.match(/([^?s=\d]([^\/][\d]+))/g)
+      if (id) return id[0]?.split('/')[1]
     }
   },
-  watch:{
+  watch: {
     query: 'fetchTweet'
   }
 })
@@ -45,27 +45,31 @@ export default defineComponent({
   </header>
   <div class="w-10/12 md:w-[45%] mt-10 mx-auto">
     <div class="w-full flex justify-between h-12">
-      <input type="search" v-model="query" class="w-full rounded-l-xl bg-green-50 border-2 border-green-400  px-4 text-xl text-green-800 focus:ring-4 focus:ring-green-100 focus:ing-opacity-50" name="URI" id="">
-      <button  class="rounded-r-xl border-2 bg-green-400 text-green-900" @click="fetchTweet" type="submit">Generate</button>
+      <input type="search" v-model="query"
+        class="w-full rounded-l-xl bg-green-50 border-2 border-green-400  px-4 text-xl text-green-800 focus:ring-4 focus:ring-green-100 focus:ing-opacity-50"
+        name="URI" id="">
+      <button class="rounded-r-xl border-2 bg-green-400 text-green-900" @click="fetchTweet"
+        type="submit">Generate</button>
     </div>
   </div>
   <section class="py-4">
-    <Peeped :tweet="payload"/>
+    <Peeped :tweet="payload" />
   </section>
 </template>
 
 <style lang="scss">
-  body {
-    @apply m-0 p-0 bg-gray-600 subpixel-antialiased;
-  }
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-  }
+body {
+  @apply m-0 p-0 bg-gray-600 subpixel-antialiased;
+}
 
-  button {
-    @apply border-transparent h-12 uppercase  font-semibold p-2;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+button {
+  @apply border-transparent h-12 uppercase font-semibold p-2;
+}
 </style>
--- src/components/Peeped.vue
+++ src/components/Peeped.vue
@@ -1,36 +1,29 @@
-<script  lang="ts">
-  import { ref, toRefs} from 'vue'
-  
-  export default {
-      props:{
-        tweet: {
-          type: Object,
-          required: true
-        }
-      },
-      setup(props) {
-        const { tweet } = toRefs(props)
-        const context = ref<HTMLElement | null>(null)
-        return {
-          context,
-          tweet,
-          props
-        }
-      },
-  }
+<script setup  lang="ts">
+import { ref, toRefs } from 'vue'
+
+interface Tweet {
+  text?: string
+  user?: string
+}
+
+const props = defineProps<{
+  tweet: Tweet
+}>()
+const { tweet } = toRefs(props)
+const context = ref<HTMLElement | null>(null)
 </script>
 <template>
-<div class="flex justify-center">
-  <div  id="peeped-tweet" ref="context" class="bg-green-700 text-emerald-300">
-    <span v-html="tweet.user"></span>
-    <section id="tweet" class="w-full h-[80%] flex flex-col justify-center align-middle text-center text-[24pt]">
-      <p  v-html="tweet.text"></p>
-    </section>
+  <div class="flex justify-center">
+    <div id="peeped-tweet" ref="context" class="bg-green-700 text-emerald-300">
+      <span v-html="tweet.user"></span>
+      <section id="tweet" class="w-full h-[80%] flex flex-col justify-center align-middle text-center text-[24pt]">
+        <p v-html="tweet.text"></p>
+      </section>
+    </div>
   </div>
-</div>
 </template>
 <style lang="scss">
-  #peeped-tweet {
-    @apply w-500px h-500px px-10 py-8 font-semibold shadow-md;
-  }
+#peeped-tweet {
+  @apply w-500px h-500px px-10 py-8 font-semibold shadow-md;
+}
 </style>
